0913a7377fa737beebbc57a9f5f2f24d57b59890
QyongGin <dydwlsdkrak03@gmail.com>
2025-03-24 15:13:51 +0900

    0.7

--- week03.py
+++ week03.py
@@ -32,10 +32,14 @@
 # print(set(duplicate_city(cities)))
 
 def inters(l1,l2):
-    pass
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append()
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
+print(inters(l1,l2))
 
 
 
