a38d4133520c10f1eccd560c4e900ff2e1b5c42a
QyongGin <dydwlsdkrak03@gmail.com>
2025-03-24 15:10:48 +0900

    v0.4array

--- week03.py
+++ week03.py
@@ -1,30 +1,41 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-ratings = [1, 2, 4, 3]
+# groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+# ratings = [1, 2, 4, 3]
+#
+# group_rating = list(zip(groups, ratings)) # zip 두 리스트 결합. 내부 반복문 돌아감. 작은 수 기준으로
+# print(group_rating)
+#
+# groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+# ratings = [1, 2]
+#
+# group_rating = list(zip(groups, ratings)) # 두 리스트 길이 확인해야 함.
+# print(group_rating)
+#
+# # set
+# # 중복이 없어진다. 딕셔너리 키만 잇는 버전.
+#
+# def duplicate_city(cities):
+#     result_city = list()
+#     s = set()
+#
+#     for city in cities:
+#         l1 = len(s)
+#         s.add(city)
+#         l2 = len(s)
+#         if l1 == l2:
+#             result_city.append(city)
+#     return result_city
+#
+# cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
+# cities.append('Seoul')
+# cities.append('Antang')
+# print(cities)
+# print(set(duplicate_city(cities)))
 
-group_rating = list(zip(groups, ratings)) # zip 두 리스트 결합. 내부 반복문 돌아감. 작은 수 기준으로
-print(group_rating)
+def inters(l1,l2):
+    pass
 
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-ratings = [1, 2]
-
-group_rating = list(zip(groups, ratings)) # 두 리스트 길이 확인해야 함.
-print(group_rating)
-
-# set
-
-def duplicate_city(cities):
-    result_city = list()
-    s = set()
-
-    for city in cities:
-        l1 =
-
-cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
-cities = set(cities) # 중복이 없어진다. 딕셔너리 키만 잇는 버전.
-cities.add('Seoul')
-cities.add('Antang')
-print(cities)
-
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
 
 
 
