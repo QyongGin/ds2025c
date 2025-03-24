a2c6daee056a987becdaa32016624018ccfa4cce
QyongGin <dydwlsdkrak03@gmail.com>
2025-03-24 14:53:50 +0900

    v0.4array

--- week03.py
+++ week03.py
@@ -1,19 +1,16 @@
-import array # 배열 비슷한 성능을 내고 싶다면 array
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+ratings = [1, 2, 4, 3]
 
-def move_zeros(arr):
-    zero_index = 0
-    for index, n in enumerate(arr):
-        if n != 0:
-            arr[zero_index] = n
-            if zero_index != index:
-                arr[index] = 0
-            zero_index += 1
-    return(arr)
+group_rating = list(zip(groups, ratings)) # zip 두 리스트 결합. 내부 반복문 돌아감. 작은 수 기준으로
+print(group_rating)
 
-arr = array.array('f',[99, 0, -7, 0, 16]) # 배열이 아니라 리스트.
-move_zeros(arr)
-print(arr)
-# 메모리 아끼기위해 일정 범위 안 동일숫자를 베껴서 주소가 같다..
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+ratings = [1, 2]
+
+group_rating = list(zip(groups, ratings)) # 두 리스트 길이 확인해야 함.
+print(group_rating)
+
+# set
 
 
 
@@ -29,7 +26,6 @@ print(arr)
 
 
 
-for i in range (len(arr)):
-    print(f"{arr[i]:5} {id(arr[i])}") # l의 i번째 칸. l이라는 리스트의 i 주소 #0은 같은 주소 사용
-    # 리스트를 배열처럼 쓸 뿐. 배열 제공 없음.
+
+
 
