32097ff66260428391e6f57467aedd453421fb13
QyongGin <dydwlsdkrak03@gmail.com>
2025-04-07 14:54:00 +0900

    v2.2 stack list

--- week05.py
+++ week05.py
@@ -40,36 +40,48 @@
 # print(s1.size()) # 1
 # print(s1.peek()) # Data structure
 
-class Node: # 링크드 리스트 활용.
-    def __init__(self, data):
-        self.data = data
-        self.link = None
+# class Node: # 링크드 리스트 활용.
+#     def __init__(self, data):
+#         self.data = data
+#         self.link = None
+#
+#
+# class Stack:
+#     def __init__(self):
+#         self.top = None # head x top
+#
+#     def push(self, data):
+#         node = Node(data)
+#         if self.top is None:
+#             self.top = node
+#         else:
+#             node.link = self.top
+#             self.top = node  # top update
+#
+#     def pop(self):
+#         if self.top is None:
+#             return "Stack is empty!"
+#         popped_node = self.top # 백업용 변수 popped_node 함수가 끝나면 지역변수니까 삭제된다.
+#         self.top = self.top.link # self.top이 가리켰던 노드의 link를 참조했다.
+#         popped_node.link = None  # 삭제할 노드의 link까지 깔끔하게 삭제
+#         return popped_node.data # 삭제하는 노드 데이터 반환.
+#
+#
+# s1 = Stack()
+# print(s1.pop())
+# s1.push("Data structure")
+# s1.push("Database")
+# print(s1.pop())
+# print(s1.pop())
 
-
-class Stack:
-    def __init__(self):
-        self.top = None # head x top
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node  # top update
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty!"
-        popped_node = self.top # 백업용 변수 popped_node 함수가 끝나면 지역변수니까 삭제된다.
-        self.top = self.top.link # self.top이 가리켰던 노드의 link를 참조했다.
-        popped_node.link = None  # 삭제할 노드의 link까지 깔끔하게 삭제
-        return popped_node.data # 삭제하는 노드 데이터 반환.
-
-
-s1 = Stack()
+s1 = list()
+print(len(s1))
+s1.append("Data structure") # push
+s1.append("Database")
+print(len(s1)) # size
+print(s1[-1]) # peek
+print(s1)
 print(s1.pop())
-s1.push("Data structure")
-s1.push("Database")
+print(s1)
 print(s1.pop())
-print(s1.pop())
+print(s1)
