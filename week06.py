7f3724eee8ccb0aa3a2cbaaf206563d461decf89
QyongGin <dydwlsdkrak03@gmail.com>
2025-04-14 14:24:45 +0900

    v2.5 Queue

--- week06.py
+++ week06.py
@@ -22,7 +22,21 @@ class Queue:
             self.rear.link = node
             self.rear = node
 
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError("Queue is empty!")
+        self.size = self.size - 1
+        temp = self.front             # 선입 저장
+        self.front = self.front.link  # 이제 선입은 다음 입력됐던 값으로
+        if self.front is None:        # 다음 값이 없다면
+            self.rear = None          # 마지막 값이 삭제됐으니 rear도 None 설정
+        return temp.data
+
 q = Queue()
 q.enqueue("Database")
 q.enqueue("Data structure")
 print(q.size, q.front.data, q.rear.data)
+q.dequeue()
+print(q.size, q.front.data, q.rear.data)
+q.dequeue()
+print(q.size, q.front, q.rear) # data가 None이 됐으니 속성이 사라졌다.
