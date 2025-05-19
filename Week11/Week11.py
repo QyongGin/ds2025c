ba76b26bade60349d59153ef1c2c7e6e6157fd35
QyongGin <dydwlsdkrak03@gmail.com>
2025-05-19 13:54:27 +0900

    v 4.2 Graph

--- Week11/Week11.py
+++ Week11/Week11.py
@@ -13,7 +13,7 @@ class Graph:
 
 
 G1 = Graph(4)
-G3 = Graph(4)
+G2 = Graph(4)
 G_self = Graph(4)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
@@ -30,15 +30,17 @@ for r in range(G1.size):
     print()
 
 
-# # 0 == A, 1 == B, 2 == C, 3 == D
-# G3.graph[0][1] = 1; G3.graph[0][2] = 1
-# G3.graph[3][0] = 1; G3.graph[3][2] = 1
-#
-# print("G3 방향 그래프")
-# for r in range(G3.SIZE):
-#     for c in range(G3.SIZE):
-#         print(G3.graph[r][c], end=' ')
-#     print()
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1 # [0,1,1,0]
+                                       # [0,0,0,0]
+G2.graph[3][0] = 1; G2.graph[3][2] = 1 # [1,0,1,0]
+
+print("G2 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
+    print()
+
 #
 # # 0 == A, 1 == B, 2 == C, 3 == D
 # G_self.graph[0][3] = 1
