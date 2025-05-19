d016a14021c69f0086c62a00c520a238474588fa
QyongGin <dydwlsdkrak03@gmail.com>
2025-05-19 14:17:02 +0900

    v 4.3 Graph

--- Week11/Week11.py
+++ Week11/Week11.py
@@ -1,55 +1,28 @@
 # Graph
 
-## Week09 인용
+## Week11
 
-class Graph:
-    def __init__(self, size):
-        self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+# 깊이 우선 탐색 DFS Deeps First Search
+# DFS Function create
 
-        # 안쪽 for문, 바깥 for문. size => node 개수
-        # 4행 4열 생성 [[0,0,0,0]...0]]
-        #
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
+def dfs(g, i, visited):
+    visited[i] = 1 # visited node visited 표시
+    print(chr(ord('A') + i ), end = ' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
 
-G1 = Graph(4)
-G2 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1 # [0,1,1,1]
-G1.graph[1][0] = 1; G1.graph[1][2] = 1                     # [1,0,1,0]
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1 # [1,1,0,1]
-G1.graph[3][0] = 1; G1.graph[3][2] = 1                     # [1,0,1,0]
-                                                           # 대칭
-
-print("G1 무방향 그래프")
-for r in range(G1.size):
-    for c in range(G1.size):
-        print(G1.graph[r][c], end=' ')
-    print()
-
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G2.graph[0][1] = 1; G2.graph[0][2] = 1 # [0,1,1,0]
-                                       # [0,0,0,0]
-G2.graph[3][0] = 1; G2.graph[3][2] = 1 # [1,0,1,0]
-
-print("G2 방향 그래프")
-for r in range(G2.size):
-    for c in range(G2.size):
-        print(G2.graph[r][c], end=' ')
-    print()
-
-#
-# # 0 == A, 1 == B, 2 == C, 3 == D
-# G_self.graph[0][3] = 1
-# G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-# G_self.graph[2][1] = 1
-# G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-#
-# print("G_self 무방향 그래프")
-# for r in range(G_self.SIZE):
-#     for c in range(G_self.SIZE):
-#         print(G_self.graph[r][c], end=' ')
-#     print()
+# v_dfs = [0,0,0,0,0,0,0,0] A,B,C,D,E,F,G,H
+visited_dfs = [ 0 for _ in range(len(graph))]
+dfs(graph, 4, visited_dfs)
